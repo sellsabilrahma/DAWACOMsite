3d358552f91b7c2692f0666e656e7da1b5f72841
sellsabilrahma <157852387+sellsabilrahma@users.noreply.github.com>
2026-03-22 00:25:13 +0100

    Update scriptpharma.js

--- scriptpharma.js
+++ scriptpharma.js
@@ -3,7 +3,7 @@ const list = document.getElementById("list");
 const span = document.getElementById("span");
 
 
-const BACKEND_URL = "https://dawa-backend.onrender.com/api/garde/COMMUNE";
+const BACKEND_URL = "https://dawa-backend.onrender.com";
 
 
 dropdownBtn.onclick = function(e) {
